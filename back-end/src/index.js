aadb123a817fb3a4af9f0e693c75733331f78b45
Isaac Caliz <isaaccaliz305@gmail.com>
2024-05-25 10:48:54 -0600

    create welcome to api

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -27,6 +27,67 @@ app.use('/api', TipoServicio);
 app.use('/api', Transferencia);
 app.use('/api', Usuario);
 
+// inicio bienvenida en la ruta raiz
+app.get('/api', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Bienvenido a la API de la aplicación de pagos</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f9;
+          color: #333;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+        }
+        .container {
+          text-align: center;
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #4CAF50;
+        }
+        p {
+          font-size: 1.2em;
+        }
+        .button {
+          display: inline-block;
+          margin-top: 20px;
+          padding: 10px 20px;
+          font-size: 1em;
+          color: #fff;
+          background-color: #4CAF50;
+          text-decoration: none;
+          border-radius: 5px;
+          transition: background-color 0.3s;
+        }
+        .button:hover {
+          background-color: #45a049;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>¡Bienvenido a la API de la aplicación de gestion de Servicios!</h1>
+        <p>Estamos encantados de tenerte aquí. Explora nuestra API para gestionar tus pagos de manera eficiente y segura.</p>
+        <a href="/docs" class="button">Ver Documentación</a>
+      </div>
+    </body>
+    </html>
+  `);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
