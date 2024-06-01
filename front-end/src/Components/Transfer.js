8b3e615a0f369824624d08c81870124087eb2862
Isaac Caliz <isaaccaliz305@gmail.com>
2024-05-31 23:39:43 -0600

    mejora en la seleccion de cuentas

--- front-end/src/Components/Transfer.js
+++ front-end/src/Components/Transfer.js
@@ -3,13 +3,15 @@ import { connectToAPI } from '../api.js';
 import Swal from 'sweetalert2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash, faSquarePlus, faListCheck } from '@fortawesome/free-solid-svg-icons';
-import Alert from '../layouts/Alert.js'; // Asegúrate de que esta ruta sea correcta
+import Alert from '../layouts/Alert.js';
 
 const TransferForm = () => {
     const [fechaTransferencia, setFechaTransferencia] = useState('');
     const [montoTransferencia, setMontoTransferencia] = useState('');
+    const [cuentas, setCuentas] = useState([]);
     const [cuentaOrigenId, setCuentaOrigenId] = useState('');
     const [cuentaDestinoId, setCuentaDestinoId] = useState('');
+    const [usuarios, setUsuarios] = useState([]);
     const [idUsuarioAutorizador, setIdUsuarioAutorizador] = useState('');
     const [transfers, setTransfers] = useState([]);
     const [editTransferId, setEditTransferId] = useState(null);
@@ -28,8 +30,36 @@ const TransferForm = () => {
         }
     };
 
+    const fetchAccounts = async () => {
+        try {
+            const response = await connectToAPI('/cuentasActivas');
+            if (response.status) {
+                setCuentas(response.data);
+            } else {
+                console.error('Error al obtener las cuentas:', response.error);
+            }
+        } catch (error) {
+            console.error('Error al conectar con la API:', error);
+        }
+    };
+
+    const fetchUsuarios = async () => {
+        try {
+            const response = await connectToAPI('/users');
+            if (response.status) {
+                setUsuarios(response.data);
+            } else {
+                console.error('Error al obtener los usuarios:', response.error);
+            }
+        } catch (error) {
+            console.error('Error al conectar con la API:', error);
+        }
+    };
+
     useEffect(() => {
         fetchTransfers();
+        fetchAccounts();
+        fetchUsuarios();
     }, []);
 
     const handleSubmit = async (e) => {
@@ -50,12 +80,10 @@ const TransferForm = () => {
                 fetchTransfers();
                 handleCancel();
             } else {
-                // Mostrar mensajes específicos del backend en SweetAlert
                 let errorMessage = 'Error en la operación';
                 if (response.message) {
                     errorMessage = response.message;
                 }
-                // Mostrar el mensaje de error en SweetAlert
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
@@ -67,7 +95,6 @@ const TransferForm = () => {
             setAlert({ type: 'error', message: 'Error al conectar con la API', show: true });
         }
     };
-
 
     const handleCancel = () => {
         setFechaTransferencia('');
@@ -109,73 +136,91 @@ const TransferForm = () => {
         <div className="flex flex-col items-center min-h-screen bg-gray-200 p-6">
             {alert.show && <Alert type={alert.type} message={alert.message} />}
             <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-lg w-4/5 mb-6">
-                <h2 className="text-2xl font-bold mb-6">
-                    <FontAwesomeIcon icon={faSquarePlus} /> Nueva Transferencia
-                </h2>
-                <div className="mb-4">
-                    <label htmlFor="fechaTransferencia" className="block text-gray-700 font-bold mb-2">
-                        Fecha de Transferencia
-                    </label>
-                    <input
-                        type="date"
-                        id="fechaTransferencia"
-                        value={fechaTransferencia}
-                        onChange={(e) => setFechaTransferencia(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="montoTransferencia" className="block text-gray-700 font-bold mb-2">
-                        Monto de Transferencia
-                    </label>
-                    <input
-                        type="number"
-                        id="montoTransferencia"
-                        value={montoTransferencia}
-                        onChange={(e) => setMontoTransferencia(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="cuentaOrigenId" className="block text-gray-700 font-bold mb-2">
-                        ID de Cuenta de Origen
-                    </label>
-                    <input
-                        type="number"
-                        id="cuentaOrigenId"
-                        value={cuentaOrigenId}
-                        onChange={(e) => setCuentaOrigenId(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="cuentaDestinoId" className="block text-gray-700 font-bold mb-2">
-                        ID de Cuenta de Destino
-                    </label>
-                    <input
-                        type="number"
-                        id="cuentaDestinoId"
-                        value={cuentaDestinoId}
-                        onChange={(e) => setCuentaDestinoId(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="idUsuarioAutorizador" className="block text-gray-700 font-bold mb-2">
-                        ID de Usuario Autorizador
-                    </label>
-                    <input
-                        type="number"
-                        id="idUsuarioAutorizador"
-                        value={idUsuarioAutorizador}
-                        onChange={(e) => setIdUsuarioAutorizador(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                        required
-                    />
+                <h2 className="text-2xl font-bold mb-4"><FontAwesomeIcon icon={faSquarePlus} /> Nueva Transferencia</h2>
+                <div className="grid grid-cols-2 gap-4 mb-4">
+                    <div>
+                        <label htmlFor="fechaTransferencia" className="block text-gray-700 font-bold mb-2">
+                            Fecha de Transferencia
+                        </label>
+                        <input
+                            type="date"
+                            id="fechaTransferencia"
+                            value={fechaTransferencia}
+                            onChange={(e) => setFechaTransferencia(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="montoTransferencia" className="block text-gray-700 font-bold mb-2">
+                            Monto de Transferencia
+                        </label>
+                        <input
+                            type="number"
+                            id="montoTransferencia"
+                            value={montoTransferencia}
+                            onChange={(e) => setMontoTransferencia(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="cuentaOrigenId" className="block text-gray-700 font-bold mb-2">
+                            Cuenta de Origen
+                        </label>
+                        <select
+                            id="cuentaOrigenId"
+                            value={cuentaOrigenId}
+                            onChange={(e) => setCuentaOrigenId(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            required
+                        >
+                            <option value="">Selecciona una cuenta</option>
+                            {cuentas.map((cuenta) => (
+                                <option key={cuenta.id_cuenta} value={cuenta.id_cuenta}>
+                                    {`${cuenta.nombre_cliente} - ${cuenta.numero_cuenta}`}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="cuentaDestinoId" className="block text-gray-700 font-bold mb-2">
+                            Cuenta de Destino
+                        </label>
+                        <select
+                            id="cuentaDestinoId"
+                            value={cuentaDestinoId}
+                            onChange={(e) => setCuentaDestinoId(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            required
+                        >
+                            <option value="">Selecciona una cuenta</option>
+                            {cuentas.map((cuenta) => (
+                                <option key={cuenta.id_cuenta} value={cuenta.id_cuenta}>
+                                    {`${cuenta.nombre_cliente} - ${cuenta.numero_cuenta}`}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="idUsuarioAutorizador" className="block text-gray-700 font-bold mb-2">
+                            Usuario Autorizador
+                        </label>
+                        <select
+                            id="idUsuarioAutorizador"
+                            value={idUsuarioAutorizador}
+                            onChange={(e) => setIdUsuarioAutorizador(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            required
+                        >
+                            <option value="">Selecciona un usuario</option>
+                            {usuarios.map((usuario) => (
+                                <option key={usuario.id_usuario} value={usuario.id_usuario}>
+                                    {`${usuario.nombre_usuario}`}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
                 </div>
                 <div className="flex justify-end">
                     <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
@@ -194,9 +239,9 @@ const TransferForm = () => {
                             <th className="px-4 py-2">ID</th>
                             <th className="px-4 py-2">Fecha de Transferencia</th>
                             <th className="px-4 py-2">Monto de Transferencia</th>
-                            <th className="px-4 py-2">ID de Cuenta de Origen</th>
-                            <th className="px-4 py-2">ID de Cuenta de Destino</th>
-                            <th className="px-4 py-2">ID de Usuario Autorizador</th>
+                            <th className="px-4 py-2">Cuenta de Origen</th>
+                            <th className="px-4 py-2">Cuenta de Destino</th>
+                            <th className="px-4 py-2">Usuario Autorizador</th>
                             <th className="px-4 py-2">Acciones</th>
                         </tr>
                     </thead>
@@ -224,4 +269,3 @@ const TransferForm = () => {
 };
 
 export default TransferForm;
-
